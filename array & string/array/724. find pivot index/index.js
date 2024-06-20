082f6571ae81c08d49eb8c1d3928cba27abbfd40
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-06-20 16:09:00 +0700

    feat: add another way to solve the exercise with O(n)

--- array & string/array/724. find pivot index/index.js
+++ array & string/array/724. find pivot index/index.js
@@ -2,20 +2,39 @@
  * @param {number[]} nums
  * @return {number}
  */
+// #1: O(n^2)
+// var pivotIndex = function(nums) {
+//   for(let i = 0; i < nums.length; i ++) {
+//       let right = 0, left = 0
+//       for(let j = 0; j < i; j++) {
+//           left += nums[j]
+//       }
+      
+//       for(let k = i + 1; k > i && k < nums.length; k ++) {
+//           right += nums[k]
+//       }
+      
+//       if(left === right) {
+//           return i
+//       } 
+//   }
+//   return -1
+// };
+
+// #2: O(n)
 var pivotIndex = function(nums) {
+  let totalSum = 0, leftSum = 0
+
+  nums.forEach(num => totalSum += num)
+
   for(let i = 0; i < nums.length; i ++) {
-      let right = 0, left = 0
-      for(let j = 0; j < i; j++) {
-          left += nums[j]
-      }
-      
-      for(let k = i + 1; k > i && k < nums.length; k ++) {
-          right += nums[k]
-      }
-      
-      if(left === right) {
-          return i
-      } 
+    if(totalSum - leftSum - nums[i] === leftSum) {
+      return i 
+    }
+    leftSum += nums[i]
   }
+
   return -1
-};
+}
+
+console.log("should return 0", pivotIndex([1, -1, 1, -1, 1]))
