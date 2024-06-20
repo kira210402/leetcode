9213951718218784b05cef5288b3647186dccaa5
Duong Kim Nam <duongkimnam2142002@gmail.com>
2024-06-20 16:07:56 +0700

    feat: Largest number at least twice of others

---
+++ array & string/array/Largest number at least of twice others/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+  let max_index = 0
+  for(let i = 0; i < nums.length; i++) {
+      if(nums[max_index] < nums[i]) max_index = i
+  }
+  
+  for(let i = 0; i < nums.length; i ++) {
+      if(nums[max_index] < nums[i] * 2) {
+          return -1
+      }
+      return max_index
+  }
+};
